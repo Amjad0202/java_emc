cb89177327a5f13766a3b924a757202c1b5ac8be
Amjad0202 <mamamjadali@gmail.com>
2024-11-10 00:03:02 +0530

    Add qa1 class with main method to read a string and an integer, then print their concatenation

---
+++ java_emc/qa1.java
@@ -0,0 +1,14 @@
+import java.util.Scanner;
+
+
+public class qa1 {
+    public static void main(String[] args) {
+        Scanner name= new Scanner(System.in);
+        String a = name.nextLine();
+        
+        Scanner Age = new Scanner(System.in);
+        int b = Age.nextInt();
+
+        System.out.println(a+b);
+    }
+}
