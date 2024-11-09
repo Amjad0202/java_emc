2e61306cb489158d1c2587636397d2c2fdf2c70d
Amjad0202 <mamamjadali@gmail.com>
2024-11-09 15:27:41 +0530

    Add contain class with main method to print container values

---
+++ java_emc/contain.java
@@ -0,0 +1,9 @@
+public class contain {
+    public static void main (String[]args){
+        int container = 50;
+        String Amjad="j";
+        String container1 = Amjad;
+        System.out.print(container);
+        System.out.print(container1);
+    }
+}
