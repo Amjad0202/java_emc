17ef5a9d3dfa57ff30faef5efc65b0ebedb898f9
Amjad0202 <mamamjadali@gmail.com>
2024-11-09 19:25:02 +0530

    Add scan class with main method to read two integers and print their sum

---
+++ java_emc/scan.java
@@ -0,0 +1,13 @@
+import java.util.Scanner;
+
+public class scan {
+    public static void main(String[] args) {
+        Scanner num1 = new Scanner(System.in);
+        int a =num1.nextInt();
+        int b= num1.nextInt();
+
+        System.out.println(a+b);
+        
+    }
+}
+//getting 2 inputs from the user and adding them together
