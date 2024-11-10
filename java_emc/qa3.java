01752ee1ff1aa9e39538868d1de4931358604b3c
Amjad0202 <mamamjadali@gmail.com>
2024-11-10 17:46:26 +0530

    Refactor qa3 class to improve variable handling and output formatting for user input

--- java_emc/qa3.java
+++ java_emc/qa3.java
@@ -1,24 +1,18 @@
-
-//4. Get Input for 3 variables: Name, Score, department. Let the user
-//enter the score for 100 Marks, you convert it for 10 and print it.
-
 import java.util.Scanner;
 
-public class qa3 {
-    public static void main (String[]args){
-        Scanner var = new Scanner(System.in);
 
-        String name = var.nextLine();
+class qa3 {
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+        String name = scan.nextLine();
+        double age = scan.nextInt();
+        scan.nextLine();
+        String department = scan.nextLine();
 
-
-        double score = var.nextInt();
-        var.nextLine();
-
-        String department = var.nextLine();
-
-        System.out.println(name);
-        System.out.println(score);
-        System.out.println(department);
-        System.out.println(score/10);
+        System.out.println("Name: " + name);
+        System.out.println("Age: " + age/10);
+        System.out.println("Department: " + department);
     }
 }
+
+//4. Get Input for 3 variables: Name, Score, department. Let the user enter the score for 100 Marks, you convert it for 10 and print it.
