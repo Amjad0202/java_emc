cf891ae2aec3160685873fc987a26953e39b9f6a
Amjad0202 <mamamjadali@gmail.com>
2024-11-10 20:51:59 +0530

    Add useif class with main method to check rain condition and print appropriate message

---
+++ java_emc/useif.java
@@ -0,0 +1,9 @@
+public class useif {
+    public static void main(String[] args) {
+        boolean rain = false;
+        if(rain)
+        System.out.println("Take an umbrella");
+        else
+        System.out.println("No need for an umbrella");
+    }
+}
