06acb6a23aab93694c38b6909575461d8bd85f07
Amjad0202 <mamamjadali@gmail.com>
2024-11-10 21:02:43 +0530

    Add comp class with main method to compare two integers and print equality message

---
+++ java_emc/comp.java
@@ -0,0 +1,13 @@
+public class comp {
+    public static void main(String[] args) {
+        int num1 = 10;
+        int num2 = 20;
+
+        if(num1==num2){
+            System.out.println("num1 and num2 are equal");
+        }
+        else{
+            System.out.println("num1 and num2 are not equal");
+        }
+    }
+}
