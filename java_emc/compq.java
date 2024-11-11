2f7acba4b43ed303e54664ef029f014a8ea4b6cb
Amjad0202 <mamamjadali@gmail.com>
2024-11-11 09:49:57 +0530

    compare two strings which saved in stack

---
+++ java_emc/compq.java
@@ -0,0 +1,10 @@
+//compare two strings which saved in stack
+
+public class compq {
+    public static void main(String[] args) {
+        String a = "one";
+        String b = "one";
+        String c=b;
+        System.out.println(a==c);
+    }
+}
