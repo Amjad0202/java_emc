ce7679530ed819fb1bc60a96a35a3cff145cf986
Amjad0202 <mamamjadali@gmail.com>
2024-11-12 19:20:12 +0530

    getting input from outside and check the value with .equal

---
+++ java_emc/compq2.java
@@ -0,0 +1,16 @@
+import java.util.Scanner;
+
+public class compq2 {
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+        String meghana = scan.nextLine();
+
+        if (meghana.equals("dead")){
+            System.out.println("Surya meets Ramya");
+        }
+
+        else{
+            System.out.println("surya weds meghana");
+        }
+    }
+}
